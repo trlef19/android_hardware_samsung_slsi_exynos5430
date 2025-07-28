220b2b08fe0d7e56b7ac5b2ffc25a1d33992328e
Lefteris T. <82327147+trlef19@users.noreply.github.com>
2025-07-28 17:37:52 +0300

    add LOCAL_PRIVATE_PLATFORM_APIS := true

--- mobicore/rootpa/Code/Android/app/Android.mk
+++ mobicore/rootpa/Code/Android/app/Android.mk
@@ -12,13 +12,13 @@ LOCAL_JNI_SHARED_LIBRARIES := libcommonpawrapper
 LOCAL_PACKAGE_NAME := RootPA
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard-project.txt
 
 APP_PIE := true
 LOCAL_32_BIT_ONLY := true
 LOCAL_DEX_PREOPT := false
-LOCAL_SDK_VERSION := current
 
 include $(BUILD_PACKAGE)
 
